4ba8097084bcb3b896ebc13eeb7cbc2f3295e239
lalita635dev-prog <lalita635.dev@gmail.com>
2026-06-15 02:15:59 +0000

    agregamos busqueda y categorías

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
-document.querySelectorAll(".card").forEach(card => {
-    card.addEventListener("mouseenter", () => {
-        card.style.transition = "0.3s";
+const searchInput = document.getElementById("searchInput");
+const categoryFilter = document.getElementById("categoryFilter");
+
+const cards = document.querySelectorAll(".card");
+
+function filterCards() {
+
+    const search = searchInput.value.toLowerCase();
+    const category = categoryFilter.value;
+
+    cards.forEach(card => {
+
+        const name =
+            card.dataset.name.toLowerCase();
+
+        const categories =
+            card.dataset.category.toLowerCase();
+
+        const matchSearch =
+            name.includes(search);
+
+        const matchCategory =
+            category === "all" ||
+            categories.includes(category);
+
+        if (matchSearch && matchCategory) {
+            card.style.display = "block";
+        } else {
+            card.style.display = "none";
+        }
+
     });
-});
+}
+
+searchInput.addEventListener("input", filterCards);
+categoryFilter.addEventListener("change", filterCards);
